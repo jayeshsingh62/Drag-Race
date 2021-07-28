bb1cee2aac13d155d5d5d8bec89e147df3e1fc0b
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-28 19:37:48 +0530

    Added the challenge and did the troubleshooting

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float tiltSpeed = 50.0f;
+    private float rotationSpeed;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // get the user's rotation input
+        rotationSpeed = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * rotationSpeed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * tiltSpeed * verticalInput * Time.deltaTime);
+    }
+}
