74c374ed73f3c9f38eb976a9bcf5fc9e163e66d2
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-28 19:41:36 +0530

    Fixed the variables for different input

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,8 +6,8 @@ public class PlayerControllerX : MonoBehaviour
 {
     private float speed = 15.0f;
     private float tiltSpeed = 50.0f;
-    private float rotationSpeed;
     private float verticalInput;
+    private float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +22,10 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // get the user's rotation input
-        rotationSpeed = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * rotationSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * tiltSpeed * verticalInput * Time.deltaTime);
