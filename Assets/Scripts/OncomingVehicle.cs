990b965245e8a3533a320427efb80f26e9d627fe
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-29 20:46:25 +0530

    Gave the oncoming vehicles a default speed

--- Assets/Scripts/OncomingVehicle.cs
+++ Assets/Scripts/OncomingVehicle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OncomingVehicle : MonoBehaviour
 {
-    public float speed;
+    private float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
