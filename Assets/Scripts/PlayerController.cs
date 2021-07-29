b1b740cf357ba9ea45e96f37409c99a3447af25b
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-29 18:42:53 +0530

    Changed the speed of vehicle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private variables
-    private float speed = 20.0f;
+    private float speed = 15.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
