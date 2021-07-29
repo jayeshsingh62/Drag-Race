cc2dab74bb1115bd6e04802f2afd837429474adb
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-29 20:20:35 +0530

    Configured the script

--- Assets/Scripts/PlayerControllerP2.cs
+++ Assets/Scripts/PlayerControllerP2.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerControllerP2 : MonoBehaviour
 {
+    // Private variables
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Public variables
+    public Camera cam1;
+    public Camera cam2;
+    public string inputID;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1.enabled = true;
+        cam2.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get player input
+        horizontalInput = Input.GetAxis("Horizontal2");
+        forwardInput = Input.GetAxis("Vertical2");
+
+        // Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotate the car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            cam1.enabled = !cam1.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
     }
 }
