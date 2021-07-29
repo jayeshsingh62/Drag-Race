f86fae68242f9ac38c733e1a479b76f295299e13
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-29 20:47:08 +0530

    Gave the oncoming vehicles a faster speed since they were too slow

--- Assets/Scripts/OncomingVehicle.cs
+++ Assets/Scripts/OncomingVehicle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OncomingVehicle : MonoBehaviour
 {
-    private float speed = 5.0f;
+    private float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
