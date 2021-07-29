3c178a2da0b9e25e403404aded4acfbbfe69c2b8
Jayesh Singh <jayeshsingh62@gmail.com>
2021-07-29 19:18:27 +0530

    Added a script for driver camera

--- Assets/Scripts/DriverCam.cs
+++ Assets/Scripts/DriverCam.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OncomingVehicle : MonoBehaviour
+public class DriverCam : MonoBehaviour
 {
-    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,6 @@ public class OncomingVehicle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
 }
