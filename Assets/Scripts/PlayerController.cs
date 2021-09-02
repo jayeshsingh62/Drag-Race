f3987d452604b7587ed430da7bb95f4dfdd47d7a
Jayesh Singh <jayeshsingh62@gmail.com>
2021-09-02 14:21:51 +0530

    make the private variable to be editable in the unity editor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private variables
-    private float speed = 15.0f;
-    private float turnSpeed = 45.0f;
+    [SerializeField] private float speed = 15.0f;
+    [SerializeField] private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
 
